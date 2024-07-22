2f0bbf150b0b7dd21ce955d0ccd08385bdca8944
Eliel <46634774+elielnfinic@users.noreply.github.com>
2024-07-22 10:00:34 +0200

    Creating safe multiplication on u128

--- field_math/src/field/field.rs
+++ field_math/src/field/field.rs
@@ -1,57 +1,101 @@
 use super::{_xgcd, field_element::FieldElement};
+use super::DEFAULT_PRIME;
 
-const DEFAULT_PRIME : u128 = 1 + 407 * ( 1 << 119 );
-
-pub struct Field{
-    p : u128
+pub struct Field {
+    p: u128,
 }
 
-impl Field{
-    pub fn new() -> Field{
-        Field{
-            p : DEFAULT_PRIME
-        }
+impl Field {
+    pub fn new() -> Field {
+        Field { p: DEFAULT_PRIME }
     }
 
-    pub fn from(p : u128) -> Field{
-        Field{
-            p
-        }
+    pub fn from(p: u128) -> Field {
+        Field { p }
     }
 
-    pub fn zero( &self ) -> FieldElement{
-        FieldElement::from(0, &self)
+    pub fn zero(&self) -> FieldElement {
+        FieldElement::from(0, self)
     }
 
-    pub fn one( &self ) -> FieldElement{
-        FieldElement::from(1, &self)
+    pub fn one(&self) -> FieldElement {
+        FieldElement::from(1, self)
     }
 
-    pub fn add( &self, a : FieldElement, b : FieldElement ) -> FieldElement{
-        FieldElement::from( (a.value + b.value) % self.p, &self )
+    pub fn add(&self, a: FieldElement, b: FieldElement) -> FieldElement {
+        FieldElement::from((a.value + b.value) % self.p, self)
     }
 
-    pub fn sub( &self, a : FieldElement, b : FieldElement ) -> FieldElement{
-        FieldElement::from( (a.value + self.p - b.value) % self.p, &self )
+    pub fn sub(&self, a: FieldElement, b: FieldElement) -> FieldElement {
+        FieldElement::from((a.value + self.p - b.value) % self.p, self)
     }
 
-    pub fn mul( &self, a : FieldElement, b : FieldElement ) -> FieldElement{
-        FieldElement::from( (a.value * b.value) % self.p, &self )
+    pub fn mul(&self, a: FieldElement, b: FieldElement) -> FieldElement {
+        FieldElement::from((a.value * b.value) % self.p, self)
     }
 
-    pub fn negate( &self, a : FieldElement ) -> FieldElement{
-        FieldElement::from( (self.p - a.value) % self.p, &self )
+    pub fn negate(&self, a: FieldElement) -> FieldElement {
+        FieldElement::from((self.p - a.value) % self.p, self)
     }
 
-    pub fn inverse( &self, a : FieldElement ) -> FieldElement{
+    pub fn inverse(&self, a: FieldElement) -> FieldElement {
         let (g, x, _) = _xgcd(a.value, self.p);
-        if g != 1{
+        if g != 1 {
             panic!("{} is not invertible", a.value);
         }
-        FieldElement::from( (x + self.p) % self.p, &self )
+        FieldElement::from((x + self.p) % self.p, self)
     }
 
-    pub fn div( &self, a : FieldElement, b : FieldElement ) -> FieldElement{
-        self.mul(a, self.inverse(b))
+    pub fn div(&self, a: FieldElement, b: FieldElement) -> FieldElement {
+        let (g, x, _) = _xgcd(b.value, self.p);
+        if g != 1 {
+            panic!("{} is not invertible", b.value);
+        }
+        FieldElement::from((a.value * x) % self.p, self)
+    }
+
+    pub fn pow(&self, a: u128, b: u128) -> FieldElement {
+        FieldElement::from(a.pow(b as u32) % self.p, self)
+    }
+
+    pub fn primitive_nth_root(&self, n: u128) -> FieldElement {
+        if self.p == 1 + 407 * (1 << 119) {
+            assert!(n <= 1 << 119 && (n & (n - 1)) == 0, "Field does not have nth root of unity where n > 2^119 or not power of two.");
+            let mut root = FieldElement::from(85408008396924667383611388730472331217, self);
+            let mut order = 1 << 119;
+            while order != n {
+                root = self.pow(root.value, 2);
+                order = order / 2;
+            }
+            return root;
+        } else {
+            panic!("Unknown field, can't return root of unity.");
+        }
+    }
+
+    pub fn sample(&self, byte_array: Vec<u8>) -> FieldElement {
+        let mut acc = 0;
+        for b in byte_array {
+            acc = (acc << 8) ^ b as u128;
+        }
+        FieldElement::from(acc % self.p, self)
+    }
+}
+
+impl Default for Field {
+    fn default() -> Field {
+        Field::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::field::field_element::FieldElement;
+
+    #[test]
+    fn test_default_field() {
+        let field : Field = Default::default();
+        assert_eq!(field.p, DEFAULT_PRIME);
     }
 }
